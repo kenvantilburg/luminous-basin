f0d89272fc8a0b85a5159f879de666815891612e
Ken Van Tilburg <kenvantilburg@gmail.com>
2021-09-01 19:40:26 -0400

    Updated yellin code
    Uploaded code used on cluster for mock data sets

---
+++ code/yellin/mc_vols.py
@@ -0,0 +1,31 @@
+############## preamble #################################
+dir_home = '/mnt/home/kvantilburg/'
+dir_notebook = dir_home+'LuminousBasin/code/yellin/'
+dir_ceph = dir_home+'ceph/LuminousBasin/yellin_vols/k10_1/'
+
+import sys
+sys.path.insert(0, dir_notebook)
+
+from load_functions import *
+from model_functions import *
+from yellin_functions import *
+
+############## monte carlo #################################
+list_mu = np.logspace(0.2,2.11,201)
+
+i_mu = np.int(sys.argv[1])
+mu = list_mu[i_mu]
+print('mu =',mu)
+N_MC = 3*10**4
+N_chunk = 1
+k = 10
+
+for i_MC in range(N_MC//N_chunk):
+    vols = monte_carlo_volumes(N=N_chunk,mu=mu,k1=k,k2=k,k3=k,k4=k)
+    df = pd.DataFrame(vols)
+    df.insert(0, 'mu', N_chunk*[mu])
+    with open(dir_ceph+'vols_k'+f'{k:02d}'+'_imu_'+f'{i_mu:03d}'+'.csv','a') as f: #open data file, in 'append mode'
+        df.to_csv(f, header=f.tell()==0, index=False)
+
+
+
---
+++ code/yellin/yellin_vols.py
@@ -0,0 +1,41 @@
+dir_home = '/mnt/home/kvantilburg/'
+dir_ceph = dir_home+'ceph/LuminousBasin/'
+dir_events = dir_ceph+'mocks/event_lists/'
+dir_source = dir_ceph+'data/products_80610202001_orbit1_chu12_spatialARF/'
+dir_production = dir_ceph+'data/production/'
+
+import sys
+#sys.path.insert(0, dir_notebook)
+
+from my_units import *
+from model_functions import * 
+from load_functions import *
+from yellin_functions import *
+
+i_mock = np.int(sys.argv[1]) # mock file number
+i_m = np.int(sys.argv[2]) # axion mass integer
+k = np.int(sys.argv[3]) # number of bins for yellin binning
+print('i_mock =',str(i_mock),'| i_m =',str(i_m))
+
+dir_proj = dir_ceph+'yellin_projections/mocks/mock_'+str(i_mock)+'/'
+
+file_proj = dir_proj+'proj/proj_'+str(i_mock)+'_'+str(i_m)+'_fid.csv'
+
+df_proj = pd.read_csv(file_proj)
+N_proj = len(df_proj)
+i_down_max = np.max([0,np.int(np.ceil(np.log(N_proj/100)/np.log(2)))])
+
+for i_down in range(0,1+i_down_max):
+    file_vol = dir_proj+'vols/vols_k'+str(k)+'_'+str(i_mock)+'_'+str(i_m)+'_d_'+f'{i_down:02d}'+'_fid.csv'
+    
+    df_proj_sample = df_proj.sample(frac=2**(-i_down))
+
+    A, edges = np.histogramdd(sample=df_proj_sample.to_numpy(),
+                              bins=4*[np.linspace(0,1,k+1)]);
+
+    vols = maximal_cuboid_volumes(A)
+
+    df_vols = pd.DataFrame([vols],columns=['V_'+str(i) for i in range(len(vols))])
+
+    with open(file_vol,'w') as f: #open data file, in 'write mode'
+        df_vols.to_csv(f, index=False)
